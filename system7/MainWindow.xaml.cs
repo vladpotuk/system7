6930ecba2a65b7e4f54c66de3f7b60fd8ec5bcf2
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-06-08 19:15:50 +0300

    update

--- system7/MainWindow.xaml.cs
+++ system7/MainWindow.xaml.cs
@@ -1,24 +1,49 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace system7
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(LimitTextBox.Text, out int limit) && limit >= 0)
+            {
+                FibonacciListBox.Items.Clear();
+                List<int> fibonacciNumbers = await Task.Run(() => GenerateFibonacci(limit));
+                foreach (int number in fibonacciNumbers)
+                {
+                    FibonacciListBox.Items.Add(number);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid non-negative integer.");
+            }
+        }
+
+        private List<int> GenerateFibonacci(int limit)
+        {
+            List<int> fibonacciNumbers = new List<int>();
+            int a = 0, b = 1;
+            fibonacciNumbers.Add(a);
+
+            while (b <= limit)
+            {
+                fibonacciNumbers.Add(b);
+                int temp = a + b;
+                a = b;
+                b = temp;
+            }
+
+            return fibonacciNumbers;
         }
     }
 }
