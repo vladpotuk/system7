3e0d64791817fe707c3d81758b6f50c76096d7ae
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-06-08 19:02:01 +0300

    update

--- system7/MainWindow.xaml.cs
+++ system7/MainWindow.xaml.cs
@@ -1,24 +1,108 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace system7
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<HorseViewModel> horses;
+        private CancellationTokenSource cancellationTokenSource;
+
+        public ObservableCollection<HorseViewModel> Horses
+        {
+            get { return horses; }
+            set { horses = value; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            InitializeHorses();
         }
+
+        private void InitializeHorses()
+        {
+            Horses = new ObservableCollection<HorseViewModel>();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Horses.Add(new HorseViewModel { Name = "Horse " + i, Distance = 0 });
+            }
+        }
+
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+
+            try
+            {
+                await Task.Run(() => StartRace(cancellationTokenSource.Token));
+            }
+            catch (TaskCanceledException)
+            {
+                // Race cancelled
+            }
+        }
+
+        private async Task StartRace(CancellationToken cancellationToken)
+        {
+            Random random = new Random();
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(500);
+
+                foreach (var horse in Horses)
+                {
+                    if (cancellationToken.IsCancellationRequested)
+                        break;
+
+                    horse.Distance += random.Next(1, 11);
+                }
+
+                foreach (var horse in Horses)
+                {
+                    if (horse.Distance >= 1000)
+                    {
+                        MessageBox.Show($"{horse.Name} wins the race!", "Race Over", MessageBoxButton.OK, MessageBoxImage.Information);
+                        cancellationTokenSource.Cancel();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public class HorseViewModel : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name"));
+            }
+        }
+
+        private int distance;
+        public int Distance
+        {
+            get { return distance; }
+            set
+            {
+                distance = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Distance"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
